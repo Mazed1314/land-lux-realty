465085ed76f2673e6df0c33949f8bf2d13347a3d
Mazed1413 <mamazed1314@gmail.com>
2024-04-27 17:37:16 +0600

    firebase hosting

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ card }) => {
   const { image, estate_title, status, area, description, id } = card;
 
   return (
-    <div className="border md:border-0 rounded-md bg-base-100 shadow-xl  animate__duration-2s animate__animated animate__slideInUp">
+    <div className="border md:border-0 rounded-md bg-base-100 shadow-xl  animate__duration-3s animate__animated animate__slideInUp">
       <figure className="">
         <img
           className="rounded-t-md w-full h-[150px] md:h-[200px]"
@@ -16,13 +16,13 @@ const Card = ({ card }) => {
       </figure>
       <div className="card-body p-2">
         <div className="">
-          <h2 className="card-title font-bold text-2xl animate__delay-1s animate__animated animate__heartBeat">
+          <h2 className="card-title font-bold text-2xl animate__delay-2s animate__animated animate__heartBeat">
             {estate_title}
           </h2>
         </div>
         <div className="flex justify-between items-center">
           <p className="flex font-semibold text-lg rounded-3xl space-x-1">
-            <div className=" font-semibold text-xl animate__delay-1s animate__animated animate__bounceInDown">
+            <div className=" font-semibold text-xl animate__delay-2s animate__animated animate__bounceInDown">
               {area}
             </div>
 
@@ -38,7 +38,7 @@ const Card = ({ card }) => {
         <div className="flex justify-center">
           <NavLink
             className={
-              "mt-4 mb-2 bg-transparent border-sky-400 text-center rounded-md text-sky-400 btn text-md md:text-lg btn-sm font-normal hover:bg-sky-400 hover:text-white animate__delay-2s animate__animated animate__flipInX "
+              "mt-4 mb-2 bg-transparent border-sky-400 text-center rounded-md text-sky-400 btn text-md md:text-lg btn-sm font-normal hover:bg-sky-400 hover:text-white animate__delay-3s animate__animated animate__flipInX "
             }
             to={`/estate-details/${id}`}
           >
